1eb0c1cc8aa7be5aa9d2bf2a66785cbef097c444
Mo <mohamad@m0.wtf>
2019-03-17 19:34:13 +0000

    alpha -command line working w/ no commands, testing for mobile

--- terminal.js
+++ terminal.js
@@ -1,7 +1,47 @@
-var commandHistory = [];
-var cmd = document.getElementById('cmd');
+let caret = '<span id="caret"></span>';
+
+var currentLine = document.getElementsByClassName('line')[0];
 
 function terminal(inp){
-  sInp = inp.value.replace(/\s/g, '&nbsp;');
-  cmd.innerHTML = 'guest@m0.wtf:/$&nbsp;' + sInp + '<span id="caret"></span>';
+
+  if(inp.value[inp.value.length -1] == '\n'){
+    var argv = inp.value.substr(0, inp.value.length -1).split(' ');
+
+    inp.value = '';
+
+    currentLine.innerHTML =currentLine.innerHTML.replace(caret,'')
+
+    process(argv);
+  }
+
+  else {
+
+    sInp = inp.value.replace(/\s/g, '&nbsp;');
+    currentLine.innerHTML = 'guest@m0.wtf:/$&nbsp;' + sInp + caret;
+
+  }
+
+}
+
+function process(argv){
+  /*
+  if(COMMANDS.includes(argv[0])){
+    //..  @todo
+  }
+  */
+  if(argv[0]){
+    echo('\n<strong>' + argv[0] + '</strong> : command not found');
+  }
+
+  currentLine = document.createElement('div');
+  currentLine.className = 'line';
+  currentLine.innerHTML = 'guest@m0.wtf:/$&nbsp;' + caret;
+  document.body.appendChild(currentLine);
+}
+
+function echo(msg){
+  var newLine = document.createElement('div');
+  newLine.className = 'line';
+  document.body.appendChild(newLine);
+  newLine.innerHTML = msg;
 }
